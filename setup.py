80853396325b20da9173d06d05b30669fc791aea
Joel Burton <joel@joelburton.com>
2018-09-08 15:16:29 -0700

    Initial upload.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup
+
+setup(name='Glide Rithm',
+      version='1.1',
+      description='Rithm themes for Glide.',
+      author='Joel Burton',
+      author_email='joel@joelburton.com',
+      url='https://github.com/joelburton/glide',
+      packages=['glide_rithm'],
+      install_requires=["Glide"],
+      include_package_data=True,
+     )
